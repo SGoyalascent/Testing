b75e95e9f42c9abdaf4a96545e956d83217f87e9
SGoyalascent <shubham@ascentspark.com>
2022-01-14 11:07:58 +0530

    test

--- test_program.c
+++ test_program.c
@@ -9,8 +9,6 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-
-#define PORT     1800
 #define MAXLINE 1024
 
 int main() {
@@ -18,54 +16,67 @@ int main() {
     unsigned char buffer[MAXLINE], recv_buffer[MAXLINE];	
     unsigned char buffer_echo[MAXLINE]={0,0,2,0,0,4,2,0,0,0,0,0,22,22,0,0,1,0x3E,0x3E};
 	struct sockaddr_in  servaddr;
-
-    const char* ip_address[1] = {"73.66.181.49"};
     int port_no = 18000;
 
-    char ip[25][256];
-/*
-    FILE *myfile = fopen("legacy.txt", "r");
-    if(myfile == NULL) {
-        printf("file not found\n");
-    }
-    for(int i = 0; i < 25; i++) {
-        fscanf(myfile, "legacy_ip :- %255s", &ip[i][0]);
-        printf("ip_%d = %s\n", i, ip[i]);
-    }
-    fclose(myfile);
-*/
-    memcpy(buffer,buffer_echo,MAXLINE);	
+    const char* ip_address[25] = { "94.130.179.247",  
+                                    "185.83.253.56",    
+                                    "139.99.155.124",   
+                                    "116.203.157.233",   
+                                    "95.183.51.104",   
+                                    "52.231.186.123",   
+                                    "45.12.222.138",   
+                                    "54.36.163.57",  
+                                    "13.234.55.11",  
+                                    "13.233.223.167",   
+                                    "134.122.54.207",   
+                                    "73.66.181.49",  
+                                    "152.67.17.219",   
+                                    "104.194.248.208",   
+                                    "188.120.241.103",   
+                                    "185.37.61.73",  
+                                    "193.7.195.250",   
+                                    "104.128.190.188",   
+                                    "73.12.182.221",   
+                                    "190.105.220.72",   
+                                    "95.179.220.72",   
+                                    "13.127.254.246",   
+                                    "104.207.148.199",   
+                                    "130.255.77.156",   
+                                    "209.205.66.24" };
 
+    memcpy(buffer,buffer_echo,MAXLINE);	
     if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
         perror("socket creation failed");
         exit(EXIT_FAILURE);
     }
     
-    memset(&servaddr, 0, sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(port_no);
-    servaddr.sin_addr.s_addr = inet_addr(ip_address[0]);
+    for(int k = 0; k < 25; k++) {
+        memset(&servaddr, 0, sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(port_no);
+        servaddr.sin_addr.s_addr = inet_addr(ip_address[k]);
 
-    int n, len=0;
-    for(int i=0;i<MAXLINE;i++){
-        len++;
-        if(buffer[i]==62 && buffer[i-1]==62){
-            break;	}
+        int n, len=0;
+        for(int i=0;i<MAXLINE;i++){
+            len++;
+            if(buffer[i]==62 && buffer[i-1]==62){
+                break;	}
+        }
+        printf("Len = %d\n", len);
+
+        sendto(sockfd, (const char *)buffer, len,
+            MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
+                sizeof(servaddr));
+        n = recvfrom(sockfd, (char *)recv_buffer, MAXLINE, 
+                    MSG_WAITALL, (struct sockaddr *) &servaddr,
+                    &len);
+        printf("n: %d\n", n);
+
+        for(int i=0;i<n;i++){	
+            printf("%d,", recv_buffer[i]);
+        }
+        printf("\n");
     }
-    printf("Len = %d\n", len);
-
-    sendto(sockfd, (const char *)buffer, len,
-        MSG_CONFIRM, (const struct sockaddr *) &servaddr, 
-            sizeof(servaddr));
-    n = recvfrom(sockfd, (char *)recv_buffer, MAXLINE, 
-                MSG_WAITALL, (struct sockaddr *) &servaddr,
-                &len);
-    printf("n: %d\n", n);
-
-    for(int i=0;i<n;i++){	
-        printf("%d,", recv_buffer[i]);
-    }
-    printf("\n");
     close(sockfd);
 
     return 0;
